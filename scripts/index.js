0ec77e1d4425aa24248aea5460e1d9fc1f62be90
Matt Randolph <mttrndlph@gmail.com>
2021-02-07 18:46:23 -0500

    Added inquirer questions

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project's title?",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "Describe your project",
+      name: "description",
+    },
+  
+    {
+      type: "input",
+      message: "How should your project be installed?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "How should your project be used?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What credits do you want to include?",
+      name: "credits",
+    },
+    {
+      type: "input",
+      message: "How can users test your project?",
+      name: "tests",
+    },
+    {
+      type: "list",
+      message: "Which type of license should your project have?",
+      name: "license",
+      choices: [
+        "Apache License 2.0",
+        "ISC License",
+        "MIT License",
+        "GNU GPLv3",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "questions",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "contact",
+    },
+  ])
+  .then((response) => {};
+    
